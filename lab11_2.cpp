9e64004642fcdd206581907a8855ceb982ae63e2
ThanakornSaokum <thanakorn01350@gmail.com>
2023-12-23 23:53:06 +0700

    lab11-2

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -58,18 +58,25 @@ int main(){
 	
 	
 	cout << "---------ORE NO TURN, DRAW!!!------------\n";
+	
 	cout << "Your cards: " << cardNames[playerCards[0]] << " " << cardNames[playerCards[1]] << "\n";
+	
 	playerScore = calScore(playerCards[0],playerCards[1],playerCards[2]);
+	
 	cout << "Your score: " << playerScore << "\n";
+	
 	do{
 		cout << "(1) Destiny draw (2) Stay, SELECT: ";
 		cin >> playerAction;
+		
 	}while(playerAction != 1 && playerAction != 2); //This line of code is not completed. You need to set the condition to do loop if user's input is not 1 or 2. 
+	
 	if(playerAction == 1){
 		//The following lines of code are not completed. Please correct it.
 		
 		playerCards[2] = drawCard();
         playerScore = calScore(playerCards[0],playerCards[1],playerCards[2]);
+        
 		cout << "Player draws the 3rd card!!!" << "\n";
 		cout << "Your 3rd card: " << cardNames[playerCards[2]] << "\n";
 		cout << "Your new score: " << playerScore << "\n";
@@ -77,23 +84,23 @@ int main(){
 	}
 	cout << "------------ Turn end -------------------\n\n";
 	
-	
-	
-	
 	cout << "---------YUGI'S TURN, DRAW!!!------------\n";
+	
 	cout << "Yugi's cards: " << cardNames[yugiCards[0]] << " " << cardNames[yugiCards[1]] << "\n";
 	yugiScore = calScore(yugiCards[0],yugiCards[1],yugiCards[2]);
 	cout << "Yugi's score: " << yugiScore << "\n";
+	
 	yugiAction = findYugiAction(yugiScore);
+	
 	if(yugiAction == 1){
 	    yugiCards[2] = drawCard();
 	    yugiScore = calScore(yugiCards[0],yugiCards[1],yugiCards[2]);
+	   
 		cout << "Yugi draws the 3rd card!!!\n";
 		cout << "Yugi's 3rd card: " << cardNames[yugiCards[2]] << "\n";
 		cout << "Yugi's new score: " << yugiScore << "\n";
 	}
 	cout << "------------ Turn end -------------------\n";
-	
 	
 	checkWinner(playerScore,yugiScore);
 }
